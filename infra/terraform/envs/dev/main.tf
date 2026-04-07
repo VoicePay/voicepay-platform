560808eba0d4c75bc043214cc9d5679ff0969af6
Emmanuel <emmanuelawodi25@gmail.com>
2026-04-07 20:42:12 +0100

    Create IAM module with least privilege roles for CI/CD, Terraform, EKS and ArgoCD

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -5,6 +5,11 @@ module "vpc" {
 }
 
 module "iam" {
-  source      = "../../modules/iam"
-  environment = var.environment
+  source                   = "../../modules/iam"
+  environment              = var.environment
+  aws_region               = var.aws_region
+  aws_account_id           = var.aws_account_id
+  github_org               = var.github_org
+  github_repo              = var.github_repo
+  github_oidc_provider_arn = var.github_oidc_provider_arn
 }
