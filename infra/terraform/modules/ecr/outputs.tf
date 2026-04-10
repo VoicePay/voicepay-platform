3e91f5890cf103c86297069580ba0961a5ac1a8c
Emmanuel <emmanuelawodi25@gmail.com>
2026-04-10 23:12:13 +0100

    Revert "Merge pull request #24 from VoicePay/fix/terraform-module-errors"
    This reverts commit 56c01a5f9d2b9545f55828e13b6729b1e33ed1b3, reversing
    changes made to d5279118414364138f079847efa5ff2db0af065a.

--- infra/terraform/modules/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_urls" {
-  description = "Map of repository name to repository URL"
-  value       = { for k, v in aws_ecr_repository.this : k => v.repository_url }
-}
-
-output "repository_arns" {
-  description = "Map of repository name to repository ARN"
-  value       = { for k, v in aws_ecr_repository.this : k => v.arn }
-}
