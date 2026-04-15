c6c1abade6c3c17ae14b18fc6124469b4d7643b8
Emmanuel <emmanuelawodi25@gmail.com>
2026-04-15 21:46:42 +0100

    Install Prometheus with metrics endpoint, alerting rules, and K8s manifests

--- services/auth/voicepay/urls.py
+++ services/auth/voicepay/urls.py
@@ -20,6 +20,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("django_prometheus.urls")),
     path("", include("voicepay_auth.urls")),
     path(
         "swagger/",
