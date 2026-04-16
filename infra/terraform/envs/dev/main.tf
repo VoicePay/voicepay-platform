84c48888c987f4ad43080158ee808a44fcb38c5c
Emmanuel <emmanuelawodi25@gmail.com>
2026-04-16 01:14:40 +0100

    Switch dev EKS nodes to public subnets for testing

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -36,7 +36,7 @@ module "eks" {
   cluster_role_arn       = module.iam.eks_cluster_role_arn
   node_role_arn          = module.iam.eks_node_role_arn
   vpc_id                 = module.vpc.vpc_id
-  subnet_ids             = module.vpc.private_subnet_ids
+  subnet_ids             = module.vpc.public_subnet_ids
   node_instance_types    = var.node_instance_types
   node_desired_size      = var.node_desired_size
   node_min_size          = var.node_min_size
